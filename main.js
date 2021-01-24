6d2dc0856b8238ddd40a2b37fbf3c255381bec9d
= <=>
2021-01-24 20:03:01 -0300

    formatação

--- main.js
+++ main.js
@@ -16,164 +16,164 @@ let checkFirstInstance = app.requestSingleInstanceLock();
 //-----------------------------------------------------------//
 //----------- Code that lock one instance at time -----------//
 if (!checkFirstInstance) {
-  app.quit();
-}else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    mainWindow.show();
-    mainWindow.focus();  
-  });
+	app.quit();
+} else {
+	app.on('second-instance', (event, commandLine, workingDirectory) => {
+		mainWindow.show();
+		mainWindow.focus();
+	});
 }
 //-----------------------------------------------------------//
 
 app.whenReady().then(() => {
-  if(!fs.existsSync('./imagens')){
-    fs.mkdirSync('./imagens');
-  }
-  //----------------------------------//
-  //----------- Tray Setup -----------//
-  tray = new Tray('./icon.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { 
-      label: '🎞Abrir🎞', 
-      click:function(){mainWindow.show();mainWindow.focus()} 
-    },
-    { 
-      label: '❌Sair❌', 
-      click:function(){force_quit = true; app.quit();} 
-    }
-  ]);
-  tray.setToolTip('Copie o link do video e pressione alt + 5 para salvar o video')
-  tray.setContextMenu(contextMenu)
-  //----------------------------------//
+	if (!fs.existsSync('./imagens')) {
+		fs.mkdirSync('./imagens');
+	}
+	//----------------------------------//
+	//----------- Tray Setup -----------//
+	tray = new Tray('./icon.png')
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: '🎞Abrir🎞',
+			click: function () { mainWindow.show(); mainWindow.focus() }
+		},
+		{
+			label: '❌Sair❌',
+			click: function () { force_quit = true; app.quit(); }
+		}
+	]);
+	tray.setToolTip('Copie o link do video e pressione alt + 5 para salvar o video')
+	tray.setContextMenu(contextMenu)
+	//----------------------------------//
 
-  globalShortcut.register('Alt+numadd', () => {
-    let videoUrl = clipboard.readText()
-    let videoId = videoUrl.split('?v=').pop().split('&')[0];
-    let file = fs.createWriteStream(`./imagens/${videoId}.jpg`)
-    var request = https.get(`https://img.youtube.com/vi/${videoId}/hq1.jpg`, function(response) {
-        response.pipe(file);
-        downloadVideo({ "url" : videoUrl, "id" : videoId })
-    });
-  })
+	globalShortcut.register('Alt+numadd', () => {
+		let videoUrl = clipboard.readText()
+		let videoId = videoUrl.split('?v=').pop().split('&')[0];
+		let file = fs.createWriteStream(`./imagens/${videoId}.jpg`)
+		var request = https.get(`https://img.youtube.com/vi/${videoId}/hq1.jpg`, function (response) {
+			response.pipe(file);
+			downloadVideo({ "url": videoUrl, "id": videoId })
+		});
+	})
 
-  createWindow();
+	createWindow();
 
-  mainWindow.on('close',(e)=>{
-    if(!force_quit){
-      e.preventDefault();
-      console.log("opa");
-      mainWindow.hide();
-    }
-  })
+	mainWindow.on('close', (e) => {
+		if (!force_quit) {
+			e.preventDefault();
+			console.log("opa");
+			mainWindow.hide();
+		}
+	})
 })
 
 
 
 
 
-function createWindow(){
-    mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 600,
-        frame: false,
-        // maxWidth: 1000,
-        // maxHeight: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-        
-    });
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 600,
+		frame: false,
+		// maxWidth: 1000,
+		// maxHeight: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
 
-    mainWindow.setMenuBarVisibility(false)
-    mainWindow.loadFile(__dirname + "/src/index.html");
-    /*mainWindow.on('window_closed', (e, item)=>{
-        if (mainWindow && !mainWindow.isDestroyed())
-            mainWindow.webContents.send('button_enable', item);
-    });*/
+	});
+
+	mainWindow.setMenuBarVisibility(false)
+	mainWindow.loadFile(__dirname + "/src/index.html");
+	/*mainWindow.on('window_closed', (e, item)=>{
+		if (mainWindow && !mainWindow.isDestroyed())
+			mainWindow.webContents.send('button_enable', item);
+	});*/
 
 }
 
-ipcMain.on('closeWindow',(e,data)=>{
-    // mainWindow.hide(); PRODUCTION
-    force_quit = true; app.quit(); //DEBUG
+ipcMain.on('closeWindow', (e, data) => {
+	// mainWindow.hide(); PRODUCTION
+	force_quit = true; app.quit(); //DEBUG
 })
-ipcMain.on('minimizeWindow',(e,data)=>{
-  mainWindow.minimize();
-})
-
-ipcMain.on('downloadVideo',(e,data)=>{
-    var request = https.get(`https://img.youtube.com/vi/${data.id}/hq1.jpg`, function(response) {
-        let file = fs.createWriteStream(`./imagens/${data.id}.jpg`)
-        response.pipe(file);
-        downloadVideo(data)
-    });
-})
-ipcMain.on('getTitle',(e,data)=>{
-  try{
-    console.log(data);
-    //const video = await youtubedl(data.url,['--format=18'])
-    youtubedl.getInfo(data.url, ['--format=18'], function(err, info) {
-      if (err) console.log(err)
-
-      let nome = info._filename.replace("-"+data.id,"");
-      nome = nome.replace(".mp4","");
-      e.sender.send('videoTitle',{title:nome});
-    })
-
-  }
-  catch(e){
-
-  }
-  
+ipcMain.on('minimizeWindow', (e, data) => {
+	mainWindow.minimize();
 })
 
-ipcMain.on('getFormats',(e,data) =>{
-  youtubedl.getInfo(data.url, function(err, info) {
-    let formatos = info.formats.map((e)=>{
-      return {
-        'format_id':e.format_id,
-        'format':e.ext,
-        'resolution':e.height
-      }
-    });
-    e.sender.send('videoFormats',{formats:formatos});
-  });
+ipcMain.on('downloadVideo', (e, data) => {
+	var request = https.get(`https://img.youtube.com/vi/${data.id}/hq1.jpg`, function (response) {
+		let file = fs.createWriteStream(`./imagens/${data.id}.jpg`)
+		response.pipe(file);
+		downloadVideo(data)
+	});
+})
+ipcMain.on('getTitle', (e, data) => {
+	try {
+		console.log(data);
+		//const video = await youtubedl(data.url,['--format=18'])
+		youtubedl.getInfo(data.url, ['--format=18'], function (err, info) {
+			if (err) console.log(err)
+
+			let nome = info._filename.replace("-" + data.id, "");
+			nome = nome.replace(".mp4", "");
+			e.sender.send('videoTitle', { title: nome });
+		})
+
+	}
+	catch (e) {
+
+	}
+
+})
+
+ipcMain.on('getFormats', (e, data) => {
+	youtubedl.getInfo(data.url, function (err, info) {
+		let formatos = info.formats.map((e) => {
+			return {
+				'format_id': e.format_id,
+				'format': e.ext,
+				'resolution': e.height
+			}
+		});
+		e.sender.send('videoFormats', { formats: formatos });
+	});
 });
 
 function downloadVideo(data) {
-    const video = youtubedl(data.url,['--format=18'],{ cwd: __dirname })
-    video.on('info', function(info) {
-        
-        let nome = info._filename.replace("-"+data.id,"");
-        let icone = `./imagens/${data.id}.jpg`
+	const video = youtubedl(data.url, ['--format=18'], { cwd: __dirname })
+	video.on('info', function (info) {
 
-        console.log('Download started');
-        console.log('filename: ' + nome);
-        console.log('size: ' + info.size);
+		let nome = info._filename.replace("-" + data.id, "");
+		let icone = `./imagens/${data.id}.jpg`
+
+		console.log('Download started');
+		console.log('filename: ' + nome);
+		console.log('size: ' + info.size);
 
 
-        video.pipe(fs.createWriteStream('./videos/'+nome))
+		video.pipe(fs.createWriteStream('./videos/' + nome))
 
-        video.on('end', function() {
-            showNotification("Download Concluido","O Download do video " + nome + " foi finalizado com sucesso", icone);
-        })
+		video.on('end', function () {
+			showNotification("Download Concluido", "O Download do video " + nome + " foi finalizado com sucesso", icone);
+		})
 
-    })
+	})
 }
 
-function showNotification (titulo, corpo, icone) {
-    const notification = {
-      icon: icone,
-      title: titulo,
-      body: corpo
-    }
-    new Notification(notification).show()
-  }
+function showNotification(titulo, corpo, icone) {
+	const notification = {
+		icon: icone,
+		title: titulo,
+		body: corpo
+	}
+	new Notification(notification).show()
+}
 
 //app.on('ready', createWindow);
 
 app.on('quit', () => rimraf("./imagens", function () { console.log("done"); }))
 
-app.on('window-all-closed', (e) => {  
-  e.preventDefault();
+app.on('window-all-closed', (e) => {
+	e.preventDefault();
 })
